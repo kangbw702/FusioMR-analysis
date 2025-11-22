7b91cef12da4ca8e7ff7032b356feece00f7db21
BOWEI KANG <kangbw702@gmail.com>
2025-11-21 18:48:24 -0600

    Update seso_ciscml.R

--- simulation/seso_ciscml.R
+++ simulation/seso_ciscml.R
@@ -27,7 +27,6 @@ b_phi = args[13]
 c_gamma = args[14]
 c_theta = args[15]
 
-
 out = NULL
 nrep = 200
 for (ii in 1:nrep) {
@@ -58,7 +57,7 @@ b_out = b_out_raw[iv_keep]; se_out = se_out_raw[iv_keep]
 b_exp = b_exp_raw[iv_keep]; se_exp = se_exp_raw[iv_keep]
 
 # run MR
-# ciscml-dp
+# ciscml
 R = diag(K)
 LD_inv = solve(R)
 b_exp_cond = LD_inv %*% b_exp; b_out_cond = LD_inv %*% b_out
@@ -70,18 +69,17 @@ ciscML_res = cismr_cML_DP(b_exp_cond, b_out_cond, Sig_exp_inv, Sig_out_inv, maxi
 ciscML0_b = ciscML_res$BIC_theta; ciscML0_se = ciscML_res$BIC_se
 ciscMLDP0_b = ciscML_res$BIC_DP_theta; ciscMLDP0_se = ciscML_res$BIC_DP_se
 
-
 # output
-tmp = c(seed = ii, K = K, Kexp = Kexp, s2_gamma_theo = (b_gamma-a_gamma)^2/12, s2_theta_theo = (b_alpha-a_alpha)^2/12, s2_gamma_emp = max(1e-3, (var(b_exp_raw) - mean(se_exp_raw)^2)), s2_theta_emp = max(1e-6, (var(b_out_raw)-mean(se_out_raw^2))), b_ciscml = ciscML0_b, b_ciscml_dp = ciscMLDP0_b, se_ciscml = ciscML0_se, se_ciscml_dp = ciscMLDP0_se, cover_ciscml = ciscML0_b + 1.96* ciscML0_se > theta & ciscML0_b - 1.96* ciscML0_se < theta, cover_ciscml_dp = ciscMLDP0_b + 1.96* ciscMLDP0_se > theta & ciscMLDP0_b - 1.96* ciscMLDP0_se < theta, ciscml = ciscML0_b + 1.96* ciscML0_se < 0 | ciscML0_b - 1.96* ciscML0_se > 0, ciscml_dp = ciscMLDP0_b + 1.96* ciscMLDP0_se < 0 | ciscMLDP0_b - 1.96* ciscMLDP0_se > 0)
+tmp = c(seed = ii, K = K, Kexp = Kexp, b_ciscml = ciscML0_b, b_ciscml_dp = ciscMLDP0_b, se_ciscml = ciscML0_se, se_ciscml_dp = ciscMLDP0_se, cover_ciscml = ciscML0_b + 1.96* ciscML0_se > theta & ciscML0_b - 1.96* ciscML0_se < theta, cover_ciscml_dp = ciscMLDP0_b + 1.96* ciscMLDP0_se > theta & ciscMLDP0_b - 1.96* ciscMLDP0_se < theta, ciscml = ciscML0_b + 1.96* ciscML0_se < 0 | ciscML0_b - 1.96* ciscML0_se > 0, ciscml_dp = ciscMLDP0_b + 1.96* ciscMLDP0_se < 0 | ciscMLDP0_b - 1.96* ciscMLDP0_se > 0)
 out = rbind(out, tmp)
 } # end if
 } # end loop
 
 out = as.data.frame(out)
 if (length(out)) {
-colnames(out) = c('seed', 'K', 'Kexp', 's2_gamma_theo', 's2_theta_theo', 's2_gamma_emp', 's2_theta_emp', 'b_ciscml', 'b_ciscml_dp', 'se_ciscml', 'se_cml_cisdp', 'cover_ciscml', 'cover_ciscml_dp', 'ciscml', 'ciscml_dp')
+colnames(out) = c('seed', 'K', 'Kexp', 'b_ciscml', 'b_ciscml_dp', 'se_ciscml', 'se_cml_cisdp', 'cover_ciscml', 'cover_ciscml_dp', 'ciscml', 'ciscml_dp')
 
-out_file_name = paste0("out/simcis2g_m_", m, "_nx_", nx, "_ny_", ny, "_bgamma_", b_gamma, "_quhp_", q_uhp, "_balpha_", b_alpha, "_theta_", theta, "_pcut_", p_cutoff, "_cgamma_", c_gamma, "_ctheta_", c_theta, "_qchp_", q_chp, "_bphi_", b_phi, "_seed_", seed_k, ".txt")
+out_file_name = paste0("out/sim_m_", m, "_nx_", nx, "_ny_", ny, "_bgamma_", b_gamma, "_quhp_", q_uhp, "_balpha_", b_alpha, "_theta_", theta, "_pcut_", p_cutoff, "_cgamma_", c_gamma, "_ctheta_", c_theta, "_qchp_", q_chp, "_bphi_", b_phi, "_seed_", seed_k, ".txt")
 fwrite(out, out_file_name)
 } else {
 cat("Empty!\n")
