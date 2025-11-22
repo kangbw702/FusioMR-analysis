36368b8bbccce310727a23d9efdad7ae0b7aa477
BOWEI KANG <kangbw702@gmail.com>
2025-11-22 03:33:23 -0600

    Update utilities.R

--- functions/utilities.R
+++ functions/utilities.R
@@ -1,1 +1,9 @@
+bern_prob <- function(rho, p, q) {
+p00 = rho * sqrt(p*q*(1-p)*(1-q)) + (1-p)*(1-q)
+p10 = 1 - q - p00
+p01 = 1 - p - p00
+p11 = p + q + p00 -  1
+p00 = max(0, p00); p10 = max(0, p10); p01 = max(0, p01); p11 = max(0, p11)
+return(c(p00, p01, p10, p11))
+}
 
