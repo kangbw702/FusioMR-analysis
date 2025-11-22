7e4a9814278758f05cb6b25cd865881fca55b08f
BOWEI KANG <kangbw702@gmail.com>
2025-11-22 03:33:11 -0600

    Update memo.R

--- simulation/memo.R
+++ simulation/memo.R
@@ -17,14 +17,6 @@ Rcpp::sourceCpp("fusiomr/gibbs_seso_uhpchp.cpp")
 Rcpp::sourceCpp("fusiomr/gibbs_joint_rcpp_nopr_pp_bound.cpp")
 Rcpp::sourceCpp("dgf/fastlm.cpp")
 
-bern_prob <- function(rho, p, q) {
-p00 = rho * sqrt(p*q*(1-p)*(1-q)) + (1-p)*(1-q)
-p10 = 1 - q - p00
-p01 = 1 - p - p00
-p11 = p + q + p00 -  1
-p00 = max(0, p00); p10 = max(0, p10); p01 = max(0, p01); p11 = max(0, p11)
-return(c(p00, p01, p10, p11))
-}
 
 args <- commandArgs(trailingOnly = T)
 param = read.csv(args[1], header = T)
